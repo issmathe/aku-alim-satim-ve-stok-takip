0cb1529d84b35487d5eaec6ca4165d372a2f5b89
issmathe <issmathe@gmail.com>
2024-01-23 20:40:40 +0300

    df

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,8 @@ import Home from "./pages/Home.jsx"
 
 import KlasAkuIslemleri from "./pages/klasAkuPage/KlasAkuIslemleri.jsx";
 import KlasAkuSatim from "./pages/klasAkuPage/KlasAkuSatim.jsx";
-
+import MutluAkuIslemleri from "./pages/mutluAkuPage/MutluAkuIslemleri.jsx"
+import MutluAkuSatim from "./pages/mutluAkuPage/MutluAkuSatim.jsx"
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
       <Route path="/klasAkuIslemleri" element={<KlasAkuIslemleri/>} /> 
       <Route path="/klasAkuSatim" element={<KlasAkuSatim/>} /> 
  
+      <Route path="/mutluAkuIslemleri" element={<MutluAkuIslemleri/>} /> 
+      <Route path="/mutluAkuSatim" element={<MutluAkuSatim/>} /> 
       </Routes>
   );
 }
