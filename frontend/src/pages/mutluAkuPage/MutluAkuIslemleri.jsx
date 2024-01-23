ca9048cc299599131569b44210401e260c358388
issmathe <issmathe@gmail.com>
2024-01-23 19:17:40 +0300

    sdf

--- frontend/src/pages/mutluAkuPage/MutluAkuIslemleri.jsx
+++ frontend/src/pages/mutluAkuPage/MutluAkuIslemleri.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../../components/header/Header';
-import MutluGoster from '../../components/mutluAku/mutluEkle/MutluGoster';
+import MutluGoster from '../../components/mutluAku/mutluEkle/MutluGoster.jsx';
 
 
 const MutluAkuIslemleri = () => {
